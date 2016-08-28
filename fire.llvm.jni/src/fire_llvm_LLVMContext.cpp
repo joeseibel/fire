c3f9a81aa803fca349b73dbd63f7363eec39ce0b
Joe Seibel <jseibel@sei.cmu.edu>
2016-08-28 00:07:21 -0400

    Added basic LLVM build support to Makefile.

--- fire.llvm.jni/src/fire_llvm_LLVMContext.cpp
+++ fire.llvm.jni/src/fire_llvm_LLVMContext.cpp
@@ -1,12 +1,13 @@
+#include "llvm/IR/LLVMContext.h"
 #include "fire_llvm_LLVMContext.h"
-#include <iostream>
-
-JNIEXPORT void JNICALL Java_fire_llvm_LLVMContext_delete(JNIEnv *env, jobject obj) {
-	std::cout << "In JNI Delete" << std::endl;
-}
-
 
 JNIEXPORT jlong JNICALL Java_fire_llvm_LLVMContext_newLLVMContext(JNIEnv *env, jclass cls) {
-	std::cout << "In JNI NEW LLVMContext" << std::endl;
-	return 0;
+	llvm::LLVMContext *context = new llvm::LLVMContext();
+	return (jlong)context;
+}
+
+JNIEXPORT void JNICALL Java_fire_llvm_LLVMContext_delete(JNIEnv *env, jobject obj) {
+	jlong pointerAddress = env->GetLongField(obj, env->GetFieldID(env->GetObjectClass(obj), "pointerAddress", "J"));
+	llvm::LLVMContext *context = (llvm::LLVMContext*)pointerAddress;
+	delete context;
 }
