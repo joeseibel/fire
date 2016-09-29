27f84240c61dad74ee34d50230d4f66b467e0f7e
Joe Seibel <jseibel@sei.cmu.edu>
2016-09-29 17:04:18 -0400

    Added null check to PointerWrapper.

--- fire.llvm/src/fire/llvm/PointerWrapper.java
+++ fire.llvm/src/fire/llvm/PointerWrapper.java
@@ -10,6 +10,9 @@ abstract class PointerWrapper {
 	private final long pointerAddress;
 	
 	protected PointerWrapper(long pointerAddress) {
+		if (pointerAddress == 0) {
+			throw new IllegalArgumentException("pointerAddress cannot be 0");
+		}
 		this.pointerAddress = pointerAddress;
 	}
 }
